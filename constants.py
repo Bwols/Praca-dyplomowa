added white mask creation. Fixed normalization in dataset

constants.py
@@ -2,7 +2,7 @@ import torch
 import os
 import torchvision
 from fire_mask_dataset import DataLoader, IMAGE_DIR, MASK_DIR
-
+import numpy as np
 
 # MODEL NAMES
 VANILLA_GAN = "GAN"
@@ -63,6 +63,12 @@ def save_image_batch(output_folder,name,images):
     images_path = os.path.join(output_folder,'{}.jpg'.format(name))
     torchvision.utils.save_image(images,images_path,nrow=4,padding=0,pad_value=0)
 
+def save_masks(output_folder,name,images):
+    images = images.view(-1, 1, 64, 64)
+    make_dir(output_folder)
+    images_path = os.path.join(output_folder, '{}.jpg'.format(name))
+    torchvision.utils.save_image(images, images_path, nrow=4, padding=0, pad_value=0)
+
 
 def save_image_batch_separate(output_folder, name,images):
     images = images.view(-1, 3, 64, 64)
@@ -92,3 +98,16 @@ def create_example_input(batch_size, model_type,device ):
         im, masks = iter(DataLoader(IMAGE_DIR, MASK_DIR, batch_size, shuffle=False).get_data_loader()).next()
         masks = masks.to(device)
         return z, masks
+
+
+def create_white_mask_tensor(image):
+    image = image.detach().cpu()
+    image = np.array(image)
+    image = np.transpose(image, (0, 2, 3, 1))
+
+    white_mask = np.where(((image[:, :, :, 0] <= 0) & (image[:, :, :, 1] <= 0) & (image[:, :, :, 2] <= 0)), 0, 1)
+
+    white_mask = torch.tensor(white_mask)
+    white_mask = white_mask.reshape(-1, 1, 64, 64)
+
+    return white_mask

generative_adversarial_nets.py
@@ -31,12 +31,14 @@ class GAN:
     def choose_generator(self):
         if self.conditional_GAN:
             self.generator = CondGenerator_UNET().to(self.device)
+
         else:
             self.generator = Generator().to(self.device)
 
     def choose_discrminator(self):
         if self.conditional_GAN:
             self.discriminator = CondDiscriminator().to(self.device)
+            self.discriminator = Discriminator().to(self.device)  # TODO tutaj usuanc
         else:
             self.discriminator = Discriminator().to(self.device)
 
@@ -73,6 +75,7 @@ class GAN:
         if not self.conditional_GAN:
             return  self.train_vanilla(data)
         else:
+            return self.train_conditional_beta(data) # TODO tu też usunąć
             return self.train_conditional(data)
 
 
@@ -139,7 +142,7 @@ class GAN:
         z = create_latent_vector(batch_size, CGAN_Z).to(self.device)
 
         fake_images = self.generator(z, masks)
-        outputs = self.discriminator(real_images, masks)
+        outputs = self.discriminator(fake_images, masks)
 
         loss = self.loss_criterion(outputs, real_labels)
         loss.backward()
@@ -147,6 +150,47 @@ class GAN:
 
         return loss.item(), d_x.item() + d_g_z.item()
 
+
+
+    def train_conditional_beta(self, data):
+        mask_loss_criterion = torch.nn.MSELoss()
+        real_images, masks = data
+        batch_size = real_images.shape[0]
+        real_images = real_images.to(self.device)
+        masks = masks.to(self.device)
+
+        z = create_latent_vector(batch_size, CGAN_Z).to(self.device)
+        fake_images = self.generator(z, masks)
+
+        self.discriminator_optimizer.zero_grad()
+
+        real_outputs = self.discriminator(real_images)
+
+        fake_outputs = self.discriminator(fake_images)
+
+        real_labels, fake_labels = create_labels(batch_size, self.device)
+        d_x = self.loss_criterion(real_outputs, real_labels)
+        d_g_z = self.loss_criterion(fake_outputs, fake_labels)
+
+        d_x.backward()
+        d_g_z.backward()
+
+        self.discriminator_optimizer.step()
+
+        self.generator_optimizer.zero_grad()
+
+        fake_images = self.generator(z, masks)
+        outputs = self.discriminator(fake_images)
+
+        fake_masks = create_white_mask_tensor(fake_images)
+        fake_masks = fake_masks.to(self.device)
+
+        mask_loss = mask_loss_criterion(fake_masks, masks)
+        loss = self.loss_criterion(outputs, real_labels) + mask_loss
+        loss.backward()
+        self.generator_optimizer.step()
+
+        return loss.item(), d_x.item() + d_g_z.item()
 
 
 

test_eval_model.py
@@ -112,7 +112,7 @@ def test_discriminator(model_type, batch_size, generator_path, discriminator_pat
 def get_discriminator_error(discriminator, image_sample, labels):
     loss_criterion = torch.nn.L1Loss()
     result = discriminator(image_sample)
-    #print(result)
+    #
     loss = loss_criterion(labels, result)
 
     return loss